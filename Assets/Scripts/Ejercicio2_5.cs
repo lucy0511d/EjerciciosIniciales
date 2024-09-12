9ed116e91a78b265fe676ae2cc9709121901e70d
lucy0511d <luxiaagzl1105@gmail.com>
2024-09-12 13:01:04 +0200

    tetete

--- Assets/Scripts/Ejercicio2_5.cs
+++ Assets/Scripts/Ejercicio2_5.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Ejercicio2_5 : MonoBehaviour
 {
+    int vidasPlayer1 = 3 , vidasPlayer2 = 2, vidasPlayer3 = 4, vidasPlayer4 = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidasPlayer2 = vidasPlayer3;
+        Debug.Log("vidasPlayer2 = " + vidasPlayer2);
+        vidasPlayer3 = vidasPlayer1;
+        Debug.Log("vidasPlayer3 = " + vidasPlayer3);
+        vidasPlayer1 = vidasPlayer4;
+        Debug.Log("vidasPlayer1 = " + vidasPlayer1);
+        vidasPlayer4 = vidasPlayer2;
+        Debug.Log("vidasPlayer4 = " + vidasPlayer4);
+
     }
 
     // Update is called once per frame
