b158838a52ece9b478d623f4d641e3f24d8693ce
lucy0511d <luxiaagzl1105@gmail.com>
2024-09-13 12:53:22 +0200

    Update Ejercicio3_4.cs

--- Assets/Scripts/Ejercicio3_4.cs
+++ Assets/Scripts/Ejercicio3_4.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Ejercicio3_4 : MonoBehaviour
 {
-    [SerializeField]int EXP, LVL;
+    [SerializeField] int EXP;
+    int LVL;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LVL = 32 + (9 * EXP / 5);
+        Debug.Log( "Nivel= " + LVL );
     }
 
     // Update is called once per frame
