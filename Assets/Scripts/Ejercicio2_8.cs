ce553320ddaa168cfc8a23e6a63c71856bbbaea7
lucy0511d <luxiaagzl1105@gmail.com>
2024-09-15 13:49:41 +0200

    fdfdf

--- Assets/Scripts/Ejercicio2_8.cs
+++ Assets/Scripts/Ejercicio2_8.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Ejercicio2_8 : MonoBehaviour
 {
-    int vida = 10, veneno;
+    float vida = 120, veneno = 0.3f, vidaActualizada;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidaActualizada = vida * (0.3f + veneno);
+        Debug.Log("Vida actualizada= " + vidaActualizada);
+        vidaActualizada = vida * (0.6f + veneno);
+        Debug.Log("Vida actualizada= " + vidaActualizada);
+        vidaActualizada = vida * (0.9f + veneno);
+        Debug.Log("Vida actualizada= " + vidaActualizada);
+        vidaActualizada = vida * (1.2f + veneno);
+        Debug.Log("Vida actualizada= " + vidaActualizada);
+        vidaActualizada = vida * (1.5f + veneno);
+        Debug.Log("Vida actualizada= " + vidaActualizada);
     }
 
     // Update is called once per frame
